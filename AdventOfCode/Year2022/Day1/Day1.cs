6e23eb057cbb8d8d2b94f8f1c3af81041c46abda
Dragomir Balinov <dragomir.balinov@nif.idrett.no>
2022-12-06 19:38:43 +0100

    feat: day 1

--- AdventOfCode/Year2022/Day1/Day1.cs
+++ AdventOfCode/Year2022/Day1/Day1.cs
@@ -1,14 +1,91 @@
 ﻿namespace AdventOfCode.Year2022.Day1;
 
+/// <summary>
+/// Calorie Counting
+/// </summary>
 public class Day1 : IDay
 {
-    public Task SolvePart1()
+    private const string FileName = "Year2022/Day1/input.txt";
+
+    public async Task SolvePart1()
     {
-        throw new NotImplementedException();
+        var maxCalories = 0;
+        var currentCalories = 0;
+
+        var lines = FileReader.GetAllLinesAsync(FileName);
+        await foreach (var line in lines)
+        {
+            if (line == string.Empty)
+            {
+                currentCalories = 0;
+            }
+            else
+            {
+                currentCalories += int.Parse(line);
+            }
+
+            if (currentCalories > maxCalories)
+            {
+                maxCalories = currentCalories;
+            }
+        }
+
+        Console.WriteLine(maxCalories);
     }
 
-    public Task SolvePart2()
+    public async Task SolvePart2()
     {
-        throw new NotImplementedException();
+        var top1Calories = 0;
+        var top2Calories = 0;
+        var top3Calories = 0;
+
+        var currentCalories = 0;
+
+        var lines = FileReader.GetAllLinesAsync(FileName);
+
+        // Append empty string to handle the last group of calories
+        lines = AppendEmptyString(lines);
+
+        await foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                if (currentCalories > top1Calories)
+                {
+                    top3Calories = top2Calories;
+                    top2Calories = top1Calories;
+                    top1Calories = currentCalories;
+                }
+                else if (currentCalories > top2Calories)
+                {
+                    top3Calories = top2Calories;
+                    top2Calories = currentCalories;
+                }
+                else if (currentCalories > top3Calories)
+                {
+                    top3Calories = currentCalories;
+                }
+
+                currentCalories = 0;
+            }
+            else
+            {
+                currentCalories += int.Parse(line);
+            }
+        }
+
+        var sum = top1Calories + top2Calories + top3Calories;
+
+        Console.WriteLine(sum);
+    }
+
+    private static async IAsyncEnumerable<string> AppendEmptyString(IAsyncEnumerable<string> lines)
+    {
+        await foreach (var line in lines)
+        {
+            yield return line;
+        }
+
+        yield return string.Empty;
     }
 }
