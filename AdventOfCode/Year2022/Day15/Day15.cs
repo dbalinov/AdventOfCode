8b4c22dd08b864bc7e1f3ea366a682b0b6da589b
Dragomir Balinov <dragomir.balinov@nif.idrett.no>
2022-12-16 01:01:16 +0100

    day 15, part 2

--- AdventOfCode/Year2022/Day15/Day15.cs
+++ AdventOfCode/Year2022/Day15/Day15.cs
@@ -12,41 +12,133 @@ internal sealed class Day15 : IDay
         var lines = File.ReadLinesAsync(FileName);
 
         const int targetLine = 2000000; // test: 10;
-        var set = new HashSet<int>();
+
+        var intervals = new List<Interval>();
 
         await foreach (var line in lines)
         {
             var points = ParseLine(line);
 
-            var sensorX = points[0];
-            var sensorY = points[1];
-            var beaconX = points[2];
-            var beaconY = points[3];
+            var interval = GetInterval(points, targetLine);
 
-            var distance = Math.Abs(sensorX - beaconX) + Math.Abs(sensorY - beaconY);
-
-            var lineDistance = Math.Abs(targetLine - sensorY);
-
-            var fromX = sensorX - distance + lineDistance;
-            var toX = sensorX + distance - lineDistance;
-
-            if (toX < fromX)
+            if (interval.End < interval.Start)
             {
                 continue;
             }
-
-            for (var i = fromX; i < toX; i++)
-            {
-                set.Add(i);
-            }
+            
+            intervals.Add(interval);
         }
 
-        Console.WriteLine(set.Count);
+        var length = MergeAll(intervals)
+            .Select(x => x.End - x.Start)
+            .Sum();
+        
+        Console.WriteLine(length);
     }
 
     public async Task SolvePart2()
     {
-        throw new NotImplementedException();
+        var lines = File.ReadLinesAsync(FileName);
+
+        const int size = 4_000_000; // 20
+
+        var allIntervals = new List<Interval>[size];
+        for (var i = 0; i < allIntervals.Length; i++)
+        {
+            allIntervals[i] = new List<Interval>();
+        }
+
+        await foreach (var line in lines)
+        {
+            var points = ParseLine(line);
+
+            for (var targetLine = 0; targetLine < size; targetLine++)
+            {
+                var interval = GetInterval(points, targetLine);
+                if (interval.End < interval.Start)
+                {
+                    continue;
+                }
+
+                var start = interval.Start <= 0 ? 0 : interval.Start;
+                var end = interval.End > size - 1 ? size -1 : interval.End;
+
+                var intervals = allIntervals[targetLine];
+
+                intervals.Add(new Interval(start, end));
+            }
+        }
+
+        for (var y = 0; y < allIntervals.Length; y++)
+        {
+            allIntervals[y] = MergeAll(allIntervals[y]);
+
+            if (allIntervals[y].Count > 1)
+            {
+                var intervals = allIntervals[y];
+                var x = intervals[0].End + 1;
+                
+                var frequency = x * 4000000d + y;
+
+                Console.WriteLine(frequency);
+                break;
+            }
+        }
+    }
+
+    private static Interval GetInterval(IList<int> points, int targetLine)
+    {
+        var sensorX = points[0];
+        var sensorY = points[1];
+        var beaconX = points[2];
+        var beaconY = points[3];
+
+        var distance = Math.Abs(sensorX - beaconX) + Math.Abs(sensorY - beaconY);
+
+        var lineDistance = Math.Abs(targetLine - sensorY);
+
+        var start = sensorX - distance + lineDistance;
+        var end = sensorX + distance - lineDistance;
+
+        return new Interval(start, end);
+    }
+
+    private static List<Interval> MergeAll(IEnumerable<Interval> intervals)
+    {
+        var ordered = intervals.OrderBy(x => x.Start).ToList();
+
+        var result = new List<Interval>();
+
+        var current = ordered[0];
+        for (var i = 1; i < ordered.Count; i++)
+        {
+            if (ordered[i].Overlaps(current))
+            {
+                current = Merge(ordered[i], current);
+            }
+            else
+            {
+                result.Add(current);
+                current = ordered[i];
+            }
+        }
+
+        result.Add(current);
+
+        return result;
+    }
+
+    private static Interval Merge(Interval int1, Interval int2)
+    {
+        if (!int1.Overlaps(int2))
+        {
+            throw new ArgumentException("Interval ranges do not overlap.");
+        }
+
+        var start = int1.Start < int2.Start ? int1.Start : int2.Start;
+        var end = int1.End > int2.End ? int1.End : int2.End;
+
+        return new Interval(start, end);
     }
 
     private static IList<int> ParseLine(ReadOnlySpan<char> line)
---
+++ AdventOfCode/Year2022/Day15/Interval.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.Year2022.Day15;
+public readonly struct Interval
+{
+    public Interval(int start, int end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public int Start { get; init; }
+    public int End { get; init; }
+
+    public bool Overlaps(Interval other)
+        => other.IsInRange(Start) || other.IsInRange(End);
+
+    public bool IsInRange(int item)
+        => item >= Start && item <= End;
+}
