e479abb4f8dedc9d33be8cf7fe96d3f3f7097102
Dragomir Balinov <dragomir.balinov@nif.idrett.no>
2022-12-18 20:45:09 +0100

    feat: day 18, part 1

---
+++ AdventOfCode/Year2022/Day18/Day18.cs
@@ -0,0 +1,66 @@
+﻿namespace AdventOfCode.Year2022.Day18;
+
+internal sealed class Day18 : IDay
+{
+    private const string FileName = "Year2022/Day18/test-input.txt";
+
+    private static readonly IReadOnlyList<Point3D> Adjacencies = new Point3D[]
+    {
+        new(1, 0, 0),
+        new(-1, 0, 0),
+        new(0, 1, 0),
+        new(0, -1, 0),
+        new(0, 0, 1),
+        new(0, 0, -1),
+    };
+
+    public async Task SolvePart1()
+    {
+        var lines = File.ReadLinesAsync(FileName);
+
+        var points = await GetPointsAsync(lines);
+
+        var allVisibleSides = 0;
+        foreach (var point1 in points)
+        {
+            var visibleSides = 6;
+
+            foreach (var point2 in points)
+            {
+                if (point1 == point2) { continue; }
+
+                foreach (var adjacency in Adjacencies)
+                {
+                    if (point1 + adjacency == point2)
+                    {
+                        visibleSides--;
+                    }
+                }
+            }
+
+            allVisibleSides += visibleSides;
+        }
+
+        Console.WriteLine(allVisibleSides);
+    }
+    
+    public async Task SolvePart2()
+    {
+    }
+
+    private static async Task<IList<Point3D>> GetPointsAsync(IAsyncEnumerable<string> lines)
+    {
+        var list = new List<Point3D>();
+
+        await foreach (var line in lines)
+        {
+            var numbers = line.Split(',').Select(int.Parse).ToArray();
+
+            var point = new Point3D(numbers[0], numbers[1], numbers[2]);
+
+            list.Add(point);
+        }
+
+        return list;
+    }
+}
---
+++ AdventOfCode/Year2022/Day18/Point3D.cs
@@ -0,0 +1,36 @@
+﻿namespace AdventOfCode.Year2022.Day18;
+
+public struct Point3D : IEquatable<Point3D>
+{
+    public Point3D(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+    public static Point3D operator +(Point3D left, Point3D right)
+        => new(left.X + right.X, left.Y + right.Y, left.Z + right.Z);
+
+    public static Point3D operator -(Point3D left, Point3D right)
+        => new(left.X - right.X, left.Y - right.Y, left.Z - right.Z);
+
+    public static bool operator ==(Point3D left, Point3D right)
+        => left.Equals(right);
+
+    public static bool operator !=(Point3D left, Point3D right)
+        => !(left == right);
+
+    public bool Equals(Point3D other)
+        => X == other.X && Y == other.Y && Z == other.Z;
+
+    public override bool Equals(object? obj)
+        => obj is Point3D other && Equals(other);
+
+    public override int GetHashCode()
+        => HashCode.Combine(X, Y, Z);
+}
