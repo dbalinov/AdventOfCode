a07ce097f86cf275cc07160068d89ad11a0bdd9c
Dragomir Balinov <dragomir.balinov@nif.idrett.no>
2023-01-03 00:21:03 +0100

    fix: add missing point file for day3

---
+++ AdventOfCode/Year2019/Day3/Point.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode.Year2019.Day3;
+
+public struct Point
+{
+    public Point(int row, int col)
+    {
+        Row = row;
+        Col = col;
+    }
+
+    public int Row { get; set; }
+    public int Col { get; set; }
+
+    public int GetManhattanDistance(Point p)
+        => Math.Abs(Col - p.Col) + Math.Abs(Row - p.Row);
+}
