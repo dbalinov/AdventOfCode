e707c481d792eebb7004209fe456cfe1108f972b
Dragomir Balinov <dragomir.balinov@nif.idrett.no>
2023-01-02 20:39:15 +0100

    feat: day 1, 2019

---
+++ AdventOfCode/Year2019/Day1/Day1.cs
@@ -0,0 +1,47 @@
+﻿namespace AdventOfCode.Year2019.Day1;
+
+internal sealed class Day1 : IDay
+{
+    private const string FileName = "Year2019/Day1/input.txt";
+
+    public string Name => "The Tyranny of the Rocket Equation";
+
+    public async Task SolvePart1()
+    {
+        var lines = File.ReadLinesAsync(FileName);
+
+        var sum = 0d;
+        await foreach (var line in lines)
+        {
+            var mass = int.Parse(line);
+
+            var fuel = CalculateFuel(mass);
+
+            sum += fuel;
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    public async Task SolvePart2()
+    {
+        var lines = File.ReadLinesAsync(FileName);
+
+        var sum = 0d;
+        await foreach (var line in lines)
+        {
+            var mass = double.Parse(line);
+            var fuel = mass;
+
+            while ((fuel = CalculateFuel(fuel)) > 0)
+            {
+                sum += fuel;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+
+    private static double CalculateFuel(double mass)
+        => Math.Floor(mass / 3.0) - 2;
+}
