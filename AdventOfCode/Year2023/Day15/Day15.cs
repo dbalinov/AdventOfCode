c09c48314866eb0968a6f771d12c1bf6569a9d6d
Dragomir Balinov <dnbalinov@gmail.com>
2024-12-01 10:22:05 +0200

    fix: use LRU cache, 2024; day 15

--- AdventOfCode/Year2023/Day15/Day15.cs
+++ AdventOfCode/Year2023/Day15/Day15.cs
@@ -24,6 +24,44 @@ internal class Day15 : IDay
      {
          var text = await File.ReadAllTextAsync(FileName);
 
+         var boxes = new Box?[256];
+         foreach (var item in text.Split(','))
+         {
+             var indexOfEquals = item.IndexOf('=');
+             if (indexOfEquals > -1)
+             {
+                 var label = item[..indexOfEquals];
+                 var hashCode = GetHashCode(label);
+                 
+                 var focalLength = int.Parse(item[(indexOfEquals + 1)..]);
+                 
+                 boxes[hashCode] ??= new(hashCode);
+                 var box = boxes[hashCode]!;
+                 box.SetLens(label, focalLength);
+             }
+             else
+             {
+                 var label = item[..^1];
+                 var hashCode = GetHashCode(label);
+                 
+                 boxes[hashCode] ??= new(hashCode);
+                 var box = boxes[hashCode]!;
+                 
+                 box.RemoveLens(label);
+             }
+         }
+         
+         var result = boxes
+            .Where(box => box is not null)
+            .Sum(box => box!.GetFocusingPower());
+         
+         Console.WriteLine(result);
+     }
+
+     public async Task SolvePart2_2()
+     {
+         var text = await File.ReadAllTextAsync(FileName);
+
          var hashMap = new List<(string label, int focalLength)>?[256];
          foreach (var item in text.Split(','))
          {
@@ -90,5 +128,93 @@ internal class Day15 : IDay
          }
 
          return currentValue;
+     }
+
+     private class Box(int box)
+     {
+         private readonly Dictionary<string, Node> _lenses = new();
+         private Node? _head;
+         private Node? _tail;
+            
+         public void SetLens(string label, int focalLength)
+         {
+             if (_lenses.TryGetValue(label, out var node))
+             {
+                 node.FocalLength = focalLength;
+             }
+             else
+             {
+                 var newNode = new Node
+                 {
+                     Label = label,
+                     FocalLength = focalLength
+                 };
+                 
+                 _lenses[label] = newNode;
+                 
+                 if (_head is null)
+                 {
+                     _head = _tail = newNode;
+                 }
+                 else
+                 {
+                     _tail!.Next = newNode;
+                     newNode.Previous = _tail;
+                     _tail = newNode;
+                 }
+             }
+         }
+
+         public void RemoveLens(string label)
+         {
+             if (!_lenses.TryGetValue(label, out var node))
+             {
+                 return;
+             }
+             
+             if (node == _head)
+             {
+                 _head = _head.Next;
+             }
+             if (node == _tail)
+             {
+                 _tail = _tail.Previous;
+             }
+                 
+             if (node.Previous is not null)
+             {
+                 node.Previous.Next = node.Next;
+             }
+             if (node.Next is not null)
+             {
+                 node.Next.Previous = node.Previous;
+             }
+             
+             _lenses.Remove(label);
+         }
+
+         public int GetFocusingPower()
+         {
+             var sum = 0;
+
+             var current = _head;
+             var index = 1;
+             while (current is not null)
+             {
+                 sum += (box + 1) * index * current.FocalLength;
+                 current = current.Next;
+                 index++;   
+             }
+
+             return sum;
+         }
+     }
+
+     private class Node
+     {
+         public Node? Next { get; set; }
+         public Node? Previous { get; set; }
+         public required string Label { get; set; }
+         public int FocalLength { get; set; }
      }
 }
